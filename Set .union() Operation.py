3d172c77d2d7cf462151fd8d17a804af495bfe65
Shareena nirmala. K <84276084+shareena-28@users.noreply.github.com>
2021-06-14 10:35:02 +0530

    Create Set .union() Operation.py

---
+++ Set .union() Operation.py
@@ -0,0 +1,18 @@
+#Set .union() Operation
+E=int(input())
+English=list(input().split())
+F=int(input())
+French = list(input().split())
+print(len(set(English)|(set(French))))
+
+"""
+Input (stdin)
+9
+1 2 3 4 5 6 7 8 9
+9
+10 1 2 3 11 21 55 6 8
+Your Output (stdout)
+13
+Expected Output
+13
+"""
